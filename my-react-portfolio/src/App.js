bdf910e3b8da0e33dffe1497148410d7813a2925
BEhnert <behnert77@gmail.com>
2021-03-12 13:39:52 -0600

    fix

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home"
 import Projects from "./components/projects"
 import Resume from "./components/Resume"
-import Wrapper from "./components/Wrapper"
+// import Wrapper from "./components/Wrapper"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
 import "./components/style.css"
@@ -12,16 +12,15 @@ function App() {
   return (
     <div>
         <NavBar />  
-        <Wrapper>
         <Router>
           <Route exact path="/" component={Home} />
-          <Route exact path="/20-react_portfolio" component={Home} />
+          <Route exact path="/treact_portfolio" component={Home} />
           <Route exact path="/projects" component={Projects} />
-          <Route exact path="/20-react_portfolio/projects" component={Projects} />
+          <Route exact path="/treact_portfolio/projects" component={Projects} />
           <Route exact path="/resume" component={Resume} />
-          <Route exact path="/20-react_portfolio//resume" component={Resume} />
+          <Route exact path="/treact_portfolio//resume" component={Resume} />
           </Router>
-          </Wrapper>
+        
         <Footer />
     </div>
   );
