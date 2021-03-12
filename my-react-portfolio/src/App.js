7a886f719a5e5384d80ef673e956012ebc5b7063
BEhnert <behnert77@gmail.com>
2021-03-12 11:24:38 -0600

    New react portfolio

---
+++ my-react-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Views/Home"
+// import Projects from "./components/Views/Projects"
+// import Resume from "./components/Views/Resume"
+// import Wrapper from "./components/Wrapper"
+// import NavBar from "./components/NavBar"
+// import Footer from "./components/Footer"
+import "./components/Views/style.css"
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
