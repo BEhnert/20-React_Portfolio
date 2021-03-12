1d0f7da882f3bce15c8e9adcfa1c74a29adc75a3
BEhnert <behnert77@gmail.com>
2021-03-12 12:24:36 -0600

    remove views

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -6,23 +6,23 @@ import Resume from "./components/Resume"
 import Wrapper from "./components/Wrapper"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
-import "./components/Views/style.css"
+import "./components/style.css"
 
 function App() {
   return (
     <div>
-      <Router>
         <NavBar />  
         <Wrapper>
+        <Router>
           <Route exact path="/" component={Home} />
           <Route exact path="/20-react_portfolio" component={Home} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/20-react_portfolio/projects" component={Projects} />
           <Route exact path="/resume" component={Resume} />
           <Route exact path="/20-react_portfolio//resume" component={Resume} />
-        </Wrapper>
+          </Router>
+          </Wrapper>
         <Footer />
-      </Router>
     </div>
   );
 }
